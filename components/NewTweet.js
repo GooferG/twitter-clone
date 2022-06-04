2513d0b52b4f8477f7737b740d7df89775290f61
Luiz M <luiz.meneghim@hotmail.com>
2022-06-03 23:39:11 -0700

    added prisma table

--- components/NewTweet.js
+++ components/NewTweet.js
@@ -8,6 +8,7 @@ export default function NewTweet() {
   //don't display if we're not logged in
   if (!session || !session.user) return null;
 
+  //display form if logged in. (tweet box)
   return (
     <form
       onSubmit={async (e) => {
@@ -18,24 +19,34 @@ export default function NewTweet() {
           return;
         }
 
-        alert(content);
+        alert(
+          fetch('api/tweet', {
+            body: JSON.stringify({
+              content,
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+          })
+        );
       }}
     >
       <div className="flex">
         <div className="flex-1 px-1 pt-2 mt-2 mr-1 ml-1">
           <textarea
-            className="border p-4 w-full text-lg font-medium bg-transparent outline-none color-primary"
-            name="content"
-            cols={50}
+            className="border rounded-lg p-4 w-full text-lg font-medium bg-transparent outline-none color-primary"
             rows={2}
+            cols={50}
             placeholder="What's happening?"
+            name="content"
             onChange={(e) => setContent(e.target.value)}
           ></textarea>
         </div>
       </div>
       <div className="flex">
         <div className="flex-1 mb-5">
-          <button className="border float-right px-8 py-2 mt-0 mr-2 font-bold rounded-full">
+          <button className="border px-8 py-2 mt-0 mr-2 font-bold rounded-full">
             Tweet
           </button>
         </div>
