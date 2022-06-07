620c474bc1df822457df67a63a907e6b508aa5b7
Luiz M <luiz.meneghim@hotmail.com>
2022-06-06 17:17:26 -0700

    display tweets from DB

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,38 @@
 const { useSession } = require('next-auth/react');
+// import { useRouter } from 'next/router';
+import prisma from 'lib/prisma';
+import { getTweets } from 'lib/data.js';
 import NewTweet from 'components/NewTweet';
+import Tweets from 'components/Tweets';
 
-export default function Home() {
+export default function Home({ tweets }) {
   const { data: session, status } = useSession();
+  const loading = status === 'loading';
+  // const router = useRouter();
 
-  if (status === 'loading') {
-    return <p>loading, bitch...</p>;
+  if (loading) {
+    return null;
   }
 
-  return <div>{session ? <NewTweet /> : <p>You are not logged in 😢 </p>}</div>;
+  if (!session) {
+    router.push('/');
+  }
+
+  return (
+    <>
+      <NewTweet />
+      <Tweets tweets={tweets} />
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  let tweets = await getTweets(prisma);
+
+  tweets = JSON.parse(JSON.stringify(tweets));
+  return {
+    props: {
+      tweets,
+    },
+  };
 }
